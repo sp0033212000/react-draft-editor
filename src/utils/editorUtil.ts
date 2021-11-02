8d983cd06393763f9ba34b72f808d4f52181b674
weiliao <sp.weiliao@gmail.com>
2021-11-02 14:56:39 +0800

    Version: 0.0.49; Fixed: the data convertor only accept valid type

--- src/utils/editorUtil.ts
+++ src/utils/editorUtil.ts
@@ -1,75 +1,80 @@
-import { convertFromRaw, convertToRaw, EditorState, RawDraftContentState } from 'draft-js'
+import {
+  convertFromRaw,
+  convertToRaw,
+  EditorState,
+  RawDraftContentState,
+} from "draft-js";
 import {
   AnnouncementEditorContent,
   AnnouncementEditorDataUnity,
   AnnouncementEditorDefaultContent,
-  AnnouncementEditorRawDataUnity
-} from '..'
-import { decorator } from '../lib/draft/decorator'
-import { isNotSet } from '../utils'
-import { v4 as uuidV4 } from 'uuid'
+  AnnouncementEditorRawDataUnity,
+} from "..";
+import { decorator } from "../lib/draft/decorator";
+import { isNotSet } from "../utils";
+import { v4 as uuidV4 } from "uuid";
 
 export const convertToServerData = (
   editorData: AnnouncementEditorContent
 ): AnnouncementEditorDefaultContent => {
-  let result: AnnouncementEditorDefaultContent = []
+  let result: AnnouncementEditorDefaultContent = [];
 
-  editorData?.forEach(data => {
-    if (data.type === 'editor') {
-      if (isNotSet(data.content)) return
+  editorData?.forEach((data) => {
+    if (data.type === "editor") {
+      if (isNotSet(data.content)) return;
       result.push({
         ...data,
-        content: convertEditorStateToServerData(data.content)
-      })
-    } else {
-      result.push(data)
+        content: convertEditorStateToServerData(data.content),
+      });
+    } else if (data.type === "image" || data.type === "multi-image") {
+      result.push(data);
     }
-  })
+  });
 
-  return result
-}
+  return result;
+};
 
 export const convertFromServerData = (
   serverData: Array<AnnouncementEditorRawDataUnity>
 ): Array<AnnouncementEditorDataUnity> => {
-  let result: Array<AnnouncementEditorDataUnity> = []
+  let result: Array<AnnouncementEditorDataUnity> = [];
 
-  serverData?.forEach(data => {
-    if (data.type === 'editor') {
-      if (isNotSet(data.content)) return
+  serverData?.forEach((data) => {
+    if (data.type === "editor") {
+      if (isNotSet(data.content)) return;
       result.push({
         ...data,
-        content: convertServerDataToEditorState(data.content)
-      })
-    } else {
-      result.push(data)
+        content: convertServerDataToEditorState(data.content),
+      });
+    } else if (data.type === "image" || data.type === "multi-image") {
+      result.push(data);
     }
-  })
-  return result
-}
+  });
+  return result;
+};
 
 export const cloneEditorData = (
   editorData: AnnouncementEditorContent
 ): AnnouncementEditorContent => {
-  return editorData.map(data => ({ ...data, id: uuidV4() }))
-}
+  return editorData.map((data) => ({ ...data, id: uuidV4() }));
+};
 
 export const convertEditorStateToServerData = (editorState: EditorState) => {
-  return convertToRaw(editorState.getCurrentContent())
-}
+  return convertToRaw(editorState.getCurrentContent());
+};
 
 export const convertServerDataToEditorState = (data: RawDraftContentState) => {
   try {
-    return EditorState.createWithContent(convertFromRaw(data), decorator)
+    return EditorState.createWithContent(convertFromRaw(data), decorator);
   } catch (e) {
-    return EditorState.createEmpty(decorator)
+    return EditorState.createEmpty(decorator);
   }
-}
+};
 
 export const isEditorEmpty = (editorState: EditorState | null): boolean => {
-  console.log(editorState)
+  console.log(editorState);
 
-  if (editorState === null) return false
-  console.log(editorState.getCurrentContent().hasText())
-  return editorState.getCurrentContent().hasText()
-}
+  if (editorState === null) return false;
+  console.log(editorState.getCurrentContent().hasText());
+  return editorState.getCurrentContent().hasText();
+};
