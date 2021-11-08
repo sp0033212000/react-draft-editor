3009b57703e05884798d1cb2634bf4064b222f57
weiliao <sp.weiliao@gmail.com>
2021-11-08 18:09:13 +0800

    Version: 0.0.53; Update: add :global back to richStyle, then change name to richStyles.module.scss

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 import "draft-js/dist/Draft.css";
 import "../../src/announcementEditor.module.scss";
-import "../../src/lib/draft/styles/richStyles.scss";
+import "../../src/lib/draft/styles/richStyles.module.scss";
 
 ReactDOM.render(
   <React.StrictMode>
