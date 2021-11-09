cba1567f662684687db962e138eb339fda06f1bf
weiliao <sp.weiliao@gmail.com>
2021-11-09 11:23:40 +0800

    Update: demo the state of matter(draft-link)

--- src/lib/draft/decorator/index.tsx
+++ src/lib/draft/decorator/index.tsx
@@ -1,40 +1,42 @@
-import { CompositeDecorator, ContentState, DraftDecorator } from 'draft-js'
-import React from 'react'
+import React from "react";
+import { CompositeDecorator, ContentState, DraftDecorator } from "draft-js";
+import styles from "./index.module.scss";
 
-const findLinkEntities: DraftDecorator['strategy'] = (
+const findLinkEntities: DraftDecorator["strategy"] = (
   contentBlock,
   callback,
   contentState
 ) => {
   contentBlock.findEntityRanges((character) => {
-    const entityKey = character.getEntity()
+    const entityKey = character.getEntity();
     return (
       entityKey !== null &&
-      contentState.getEntity(entityKey).getType() === 'LINK'
-    )
-  }, callback)
-}
+      contentState.getEntity(entityKey).getType() === "LINK"
+    );
+  }, callback);
+};
 
 const DraftLink: React.FC<{ contentState: ContentState; entityKey: string }> =
   ({ contentState, entityKey, children }) => {
-    const { url } = contentState.getEntity(entityKey).getData()
+    const { url } = contentState.getEntity(entityKey).getData();
 
     return (
       <a
         href={url}
-        style={{ color: '#16C0C0', textDecoration: 'underline' }}
+        className={styles["draft-link"]}
+        style={{ color: "#16C0C0", textDecoration: "underline" }}
         title={url}
-        rel={'noreferrer noopener'}
-        target={'_blank'}
+        rel={"noreferrer noopener"}
+        target={"_blank"}
       >
         {children}
       </a>
-    )
-  }
+    );
+  };
 
 export const decorator = new CompositeDecorator([
   {
     strategy: findLinkEntities,
     component: DraftLink,
   },
-])
+]);
