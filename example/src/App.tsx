fd1d1764d8a3c6e652a4a2f8445ab50578ec6fda
weiliao <sp.weiliao@gmail.com>
2021-11-16 11:00:49 +0800

    Version: 0.0.61; Normalize the paste behavior of editor

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -40,135 +40,20 @@
 // export default App
 
 import React, { useState } from "react";
-import AnnouncementEditor, {
-  AnnouncementEditorContent,
-  convertFromServerData,
-} from "../../src";
+import { CEditor } from "../../src";
+import { EditorState } from "draft-js";
 
 const App = () => {
-  const [content] = useState<AnnouncementEditorContent>(
-    convertFromServerData([
-      {
-        id: "5ec7af25-70ca-48fe-a3d4-fe96de77101d",
-        content: {
-          blocks: [
-            {
-              key: "br1n",
-              text: "tttest",
-              type: "primary-heading",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-          ],
-          entityMap: {},
-        },
-        contentType: "primary-heading",
-        type: "editor",
-      },
-      {
-        id: "0bda646a-4f27-4943-aa93-5985d5163619",
-        content: {
-          blocks: [
-            {
-              key: "2m3vr",
-              text: "sssssssss",
-              type: "unstyled",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-            {
-              key: "blfd7",
-              text: "ddddd",
-              type: "ordered-list-item",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-            {
-              key: "8c446",
-              text: "ffffffeee",
-              type: "ordered-list-item",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-            {
-              key: "fpunf",
-              text: "ssssssss",
-              type: "unordered-list-item",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-            {
-              key: "d3n46",
-              text: "ddddd",
-              type: "unordered-list-item",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-          ],
-          entityMap: {},
-        },
-        contentType: "primary-body",
-        type: "editor",
-      },
-      {
-        id: "afb82240-fce1-4293-abbc-3131610a3be8",
-        content: {
-          blocks: [
-            {
-              key: "37sti",
-              text: "eeeeee",
-              type: "unstyled",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-            {
-              key: "7o10o",
-              text: "eeeeeeee",
-              type: "unordered-list-item",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-            {
-              key: "2jpj8",
-              text: "ssssss",
-              type: "ordered-list-item",
-              depth: 0,
-              inlineStyleRanges: [],
-              entityRanges: [],
-              data: {},
-            },
-          ],
-          entityMap: {},
-        },
-        contentType: "secondary-body",
-        type: "editor",
-      },
-      {
-        id: "5ba71704-003e-4000-ab2e-f571f68e596c",
-        content: [],
-        contentType: "twins",
-        type: "multi-image",
-      },
-    ])
-  );
+  const [editorState, setEditorState] = useState<EditorState | null>(null);
 
-  return <AnnouncementEditor readOnly content={content} attachment={[]} />;
+  return (
+    <CEditor
+      fixedToolbar
+      styleType={"primary-body"}
+      editorState={editorState}
+      setEditorState={setEditorState}
+    />
+  );
 };
 
 export default App;
