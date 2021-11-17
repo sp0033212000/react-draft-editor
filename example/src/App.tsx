c7f72ffb028d31463a2bc50aa2856b23740acf02
weiliao <sp.weiliao@gmail.com>
2021-11-17 15:32:19 +0800

    Version: 0.0.62; add new image type - thin image

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,59 +1,207 @@
-// import React, { useEffect, useState } from 'react'
-// import AnnouncementEditor, {
-//   AnnouncementEditorContent,
-//   AttachmentContentData,
-//   convertFromServerData,
-// } from '../../src'
-//
-// // style
-// import './style.scss'
-//
-// const App = () => {
-//   const [content, setContent] = useState<AnnouncementEditorContent>([])
-//   const [attachments, setAttachments] = useState<Array<AttachmentContentData>>(
-//     []
-//   )
-//
-//   // 沒有用到multiple-image的正常，可以試試下面的文章ID換122
-//   useEffect(() => {
-//     fetch(
-//       'http://tsmc-ewc-web-dev.cloud-interactive.com/tsmcWelfare/api/announcement/121'
-//     )
-//       .then((data) => data.json())
-//       .then((response) => {
-//         console.log(response.result.data.content)
-//
-//         setContent(
-//           convertFromServerData(JSON.parse(response.result.data.content))
-//         )
-//         setAttachments(response.result.data.attachLists)
-//       })
-//   }, [])
-//
-//   return (
-//     <div className="main-theme-padding">
-//       <AnnouncementEditor content={content} attachment={attachments} readOnly />
-//     </div>
-//   )
-// }
-//
-// export default App
-
 import React, { useState } from "react";
-import { CEditor } from "../../src";
-import { EditorState } from "draft-js";
+import AnnouncementEditor, {
+  AnnouncementEditorContent,
+  AttachmentContentData,
+} from "../../src";
+
+// style
+import "./style.scss";
 
 const App = () => {
-  const [editorState, setEditorState] = useState<EditorState | null>(null);
+  const [content, setContent] = useState<AnnouncementEditorContent>([
+    {
+      type: "multi-image",
+      contentType: "triplet-thin",
+      id: "123",
+      content: [
+        {
+          body: "",
+          image: { url: "", name: "" },
+          title: "",
+        },
+        {
+          body: "",
+          image: { url: "", name: "" },
+          title: "",
+        },
+      ],
+    },
+    {
+      type: "multi-image",
+      contentType: "twins-thin",
+      id: "123",
+      content: [
+        {
+          body: "",
+          image: { url: "", name: "" },
+          title: "",
+        },
+        {
+          body: "",
+          image: { url: "", name: "" },
+          title: "",
+        },
+      ],
+    },
+  ]);
+  const [attachments, setAttachments] = useState<Array<AttachmentContentData>>(
+    []
+  );
+
+  // 沒有用到multiple-image的正常，可以試試下面的文章ID換122
+  // useEffect(() => {
+  //   fetch(
+  //     "http://tsmc-ewc-web-dev.cloud-interactive.com/tsmcWelfare/api/announcement/121"
+  //   )
+  //     .then((data) => data.json())
+  //     .then((response) => {
+  //       console.log(response.result.data.content);
+  //
+  //       setContent(
+  //         convertFromServerData(JSON.parse(response.result.data.content))
+  //       );
+  //       setAttachments(response.result.data.attachLists);
+  //     });
+  // }, []);
 
   return (
-    <CEditor
-      fixedToolbar
-      styleType={"primary-body"}
-      editorState={editorState}
-      setEditorState={setEditorState}
-    />
+    <div className="main-theme-padding">
+      <AnnouncementEditor content={content} attachment={attachments} readOnly />
+    </div>
   );
 };
 
 export default App;
+
+// import React, { useState } from "react";
+// import { CEditor } from "../../src";
+// import {
+//   convertFromRaw,
+//   convertToRaw,
+//   EditorState,
+//   RawDraftContentState,
+// } from "draft-js";
+// import { decorator } from "../../src/lib/draft/decorator";
+//
+// const MOCK_DATA: RawDraftContentState = {
+//   blocks: [
+//     {
+//       key: "8oqag",
+//       text: "123123123123123",
+//       type: "primary-body",
+//       depth: 0,
+//       inlineStyleRanges: [],
+//       entityRanges: [
+//         {
+//           offset: 0,
+//           length: 15,
+//           key: 0,
+//         },
+//       ],
+//       data: {},
+//     },
+//     {
+//       key: "e1u7d",
+//       text: "",
+//       type: "primary-body",
+//       depth: 0,
+//       inlineStyleRanges: [],
+//       entityRanges: [],
+//       data: {},
+//     },
+//     {
+//       key: "6kl96",
+//       text: "qweqweqweqweqw",
+//       type: "primary-body",
+//       depth: 0,
+//       inlineStyleRanges: [
+//         {
+//           offset: 0,
+//           length: 14,
+//           style: "PRIMARY_RED_1",
+//         },
+//       ],
+//       entityRanges: [
+//         {
+//           offset: 0,
+//           length: 14,
+//           key: 1,
+//         },
+//       ],
+//       data: {},
+//     },
+//     {
+//       key: "8c320",
+//       text: "",
+//       type: "primary-body",
+//       depth: 0,
+//       inlineStyleRanges: [],
+//       entityRanges: [],
+//       data: {},
+//     },
+//     {
+//       key: "hpk6",
+//       text: "13123123123123123",
+//       type: "primary-body",
+//       depth: 0,
+//       inlineStyleRanges: [
+//         {
+//           offset: 6,
+//           length: 7,
+//           style: "PRIMARY_RED_1",
+//         },
+//       ],
+//       entityRanges: [
+//         {
+//           offset: 0,
+//           length: 17,
+//           key: 2,
+//         },
+//       ],
+//       data: {},
+//     },
+//   ],
+//   entityMap: {
+//     "0": {
+//       type: "LINK",
+//       mutability: "MUTABLE",
+//       data: {
+//         url: "http://google.com",
+//       },
+//     },
+//     "1": {
+//       type: "LINK",
+//       mutability: "MUTABLE",
+//       data: {
+//         url: "http://google.com",
+//       },
+//     },
+//     "2": {
+//       type: "LINK",
+//       mutability: "MUTABLE",
+//       data: {
+//         url: "http://google.com",
+//       },
+//     },
+//   },
+// };
+//
+// const App = () => {
+//   const [editorState, setEditorState] = useState<EditorState | null>(
+//     EditorState.createWithContent(convertFromRaw(MOCK_DATA), decorator)
+//   );
+//
+//   console.log(editorState && convertToRaw(editorState.getCurrentContent()));
+//
+//   return (
+//     <CEditor
+//       fixedToolbar
+//       styleType={"primary-body"}
+//       editorState={editorState}
+//       setEditorState={setEditorState}
+//       readOnly
+//     />
+//   );
+// };
+//
+// export default App;
--- src/types/editor/content.ts
+++ src/types/editor/content.ts
@@ -1,44 +1,46 @@
-import { EditorState } from 'draft-js'
-import { EditorDefaultStyleType } from '../../lib/draft/component/feature/cEditor'
+import {EditorState} from "draft-js";
+import {EditorDefaultStyleType} from "../../lib/draft/component/feature/cEditor";
 
 // Content Data Interface
 export interface AnnouncementEditorCEditorData {
-  id: string
-  type: 'editor'
-  contentType: EditorDefaultStyleType
-  content: EditorState | null
+  id: string;
+  type: "editor";
+  contentType: EditorDefaultStyleType;
+  content: EditorState | null;
 }
 
 export interface EditorImageContentData {
-  url: string
-  name: string
+  url: string;
+  name: string;
 }
+
 export interface AnnouncementEditorImageData {
-  id: string
-  type: 'image'
-  contentType: 'image'
-  content: EditorImageContentData | null
+  id: string;
+  type: "image";
+  contentType: "image";
+  content: EditorImageContentData | null;
 }
 
 export interface ImageWithDescriptionContentData {
-  image: EditorImageContentData
-  title: string
-  body: string
+  image: EditorImageContentData;
+  title: string;
+  body: string;
 }
+
 export interface AnnouncementEditorMultiImageData {
-  id: string
-  type: 'multi-image'
-  contentType: 'twins' | 'triplet'
-  content: Array<ImageWithDescriptionContentData>
+  id: string;
+  type: "multi-image";
+  contentType: "twins" | "triplet" | "twins-thin" | "triplet-thin";
+  content: Array<ImageWithDescriptionContentData>;
 }
 
 export interface AttachmentContentData {
-  attachName: string
-  attachUrl: string
+  attachName: string;
+  attachUrl: string;
 }
 
 export type AnnouncementEditorDataUnity =
   | AnnouncementEditorCEditorData
   | AnnouncementEditorImageData
-  | AnnouncementEditorMultiImageData
-export type AnnouncementEditorContent = Array<AnnouncementEditorDataUnity>
+  | AnnouncementEditorMultiImageData;
+export type AnnouncementEditorContent = Array<AnnouncementEditorDataUnity>;
