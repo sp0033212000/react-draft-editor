9a3e6d862c1179a26d638240bdddc628c5b46e03
Yuni Chou <yuni.chou@cloud-interactive.com>
2021-11-24 09:47:44 +0800

    #comment: update: add break-word style

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import AnnouncementEditor, {
   AnnouncementEditorContent,
   AttachmentContentData,
   convertFromServerData,
-} from "../../src";
+} from '../../src'
 
 // style
-import "./style.scss";
+import './style.scss'
 
 const App = () => {
   const [content, setContent] = useState<AnnouncementEditorContent>(
@@ -15,10 +15,10 @@ const App = () => {
         '[{"id":"f1d00378-e6fc-450d-a01a-26fd3e87641a","content":{"blocks":[{"key":"2mgp0","text":"","type":"primary-heading","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}},"contentType":"primary-heading","type":"editor"},{"id":"a4cf1f5a-366c-49aa-8331-87a4779f3783","content":[{"image":{"url":"http://tsmc-ewc-admin-sit.cloud-interactive.com/file/attachments/狗_1637142333085.jpg","name":"狗.jpg"},"title":"標題\\n第二行","body":"標題\\n第二行"},{"image":{"url":"http://tsmc-ewc-admin-sit.cloud-interactive.com/file/attachments/老鼠_1637142337744.jfif","name":"老鼠.jfif"},"title":"標題","body":"標題"}],"contentType":"twins","type":"multi-image"},{"id":"76bb51ff-fa69-41d2-882d-002a40624c7d","content":[{"image":null,"title":"標題","body":"標題"},{"image":null,"title":"標題","body":"標題"},{"image":null,"title":"標題","body":"標題"}],"contentType":"triplet","type":"multi-image"}]'
       )
     )
-  );
+  )
   const [attachments, setAttachments] = useState<Array<AttachmentContentData>>(
     []
-  );
+  )
 
   // 沒有用到multiple-image的正常，可以試試下面的文章ID換122
   // useEffect(() => {
@@ -40,10 +40,10 @@ const App = () => {
     <div className="main-theme-padding">
       <AnnouncementEditor content={content} attachment={attachments} readOnly />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
 //
 // import React, { useState } from "react";
 // import { CEditor } from "../../src";
